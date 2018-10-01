2041340a6ac5800ba6e449d4a308130ea131706c
Hermitolino Isolatorio <hermitolino@gmail.com>
2018-10-01 09:22:35 -0300

    Pomodoro's type added on project
    Added the class related to pomodoros types. That's include routes,
    controllers, models and configs
    
    The auth config is added

---
+++ api/models/pomodoro_type.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const PomodoroTypeSchema = mongoose.Schema({
+    title: String,
+    time: String
+}, {timestamps: true})
+
+module.exports = mongoose.model('PomodoroType', PomodoroTypeSchema)
---
+++ api/routes/pomodoro_types.route.js
@@ -0,0 +1,19 @@
+module.exports = (app) => {
+    const auth = require('../middlewares/auth.middleware')
+    const pomodoro_types = require('../controllers/pomodoro_types.controller.js')
+
+    // Create a new pomodoro_type
+    app.post('/pomodoro_types', auth.verifyToken, pomodoro_types.create)
+
+    // Retrive all pomodoro_types
+    app.get('/pomodoro_types', auth.verifyToken, pomodoro_types.findAll)
+
+    // Retrive a sigle pomodoro_type with id
+    app.get('/pomodoro_types/:id', auth.verifyToken, pomodoro_types.findOne)
+
+    // Update a pomodoro_type with id
+    app.put('/pomodoro_types/:id', auth.verifyToken, pomodoro_types.update)
+
+    // Delete a pomodoro_type with id
+    app.delete('/pomodoro_types/:id', auth.verifyToken, pomodoro_types.delete)
+}
